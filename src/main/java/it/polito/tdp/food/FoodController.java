f612137c2c3d3bbe0e34044b3a7098367e51a36b
Claudia Narratone <cnarratone@alice.it>
2020-06-22 11:27:50 +0200

    prima parte punto 1 fatto

--- src/main/java/it/polito/tdp/food/FoodController.java
+++ src/main/java/it/polito/tdp/food/FoodController.java
@@ -5,8 +5,10 @@
 package it.polito.tdp.food;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.food.model.Food;
 import it.polito.tdp.food.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -41,7 +43,7 @@ public class FoodController {
     private Button btnSimula; // Value injected by FXMLLoader
 
     @FXML // fx:id="boxFood"
-    private ComboBox<?> boxFood; // Value injected by FXMLLoader
+    private ComboBox<Food> boxFood; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
@@ -49,7 +51,15 @@ public class FoodController {
     @FXML
     void doCreaGrafo(ActionEvent event) {
     	txtResult.clear();
-    	txtResult.appendText("Creazione grafo...");
+    	String portionsString=txtPorzioni.getText();
+    	try {
+    		int portions=Integer.parseInt(portionsString);
+        	List<Food> cibi=model.getFoods(portions);
+        	boxFood.getItems().clear();
+        	boxFood.getItems().addAll(cibi);
+		} catch (NumberFormatException e) {
+			txtResult.appendText("Devi inserire un numero!");
+		}
     }
     
     @FXML
--- src/main/java/it/polito/tdp/food/model/Model.java
+++ src/main/java/it/polito/tdp/food/model/Model.java
@@ -1,5 +1,20 @@
 package it.polito.tdp.food.model;
 
-public class Model {
+import java.util.List;
 
+import it.polito.tdp.food.db.FoodDao;
+
+public class Model {
+	
+	private List<Food> cibi;
+	private FoodDao dao;
+	
+	public Model() {
+		dao= new FoodDao();
+	}
+
+	public List<Food> getFoods(int portions) {
+		this.cibi=(List<Food>) dao.getFoodsByPortions(portions);
+		return this.cibi;
+	}
 }
